62d0b0012189f27f1cecab8c455b6494714ca7a0
pbushko <pbushko@gmail.com>
2018-06-08 18:34:16 -0400

    Sprites for foods made appear on the plate
    After creating a dish, the food appears in your hand on a plate.  The "slop" plate is just a sort of null character at the moment.

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -14,6 +14,9 @@ public class PlayerScript : MonoBehaviour {
     //stores the items CURRENTLY in the player's hand; this doesn't count any items that are in the queue
     List<Ingreds> itemsInHand = new List<Ingreds>();
 
+    List<Sprite> foods;
+    List<string> foodNames = new List<string>();
+
     static Recipe plateInHand = null;
 
     //stores all the recipes
@@ -24,8 +27,19 @@ public class PlayerScript : MonoBehaviour {
 
     Recipe slop = new Recipe("slop", null, CookingUten.none);
 
+    SpriteRenderer plate;
+
     // Use this for initialization
     void Start () {
+        foods = new List<Sprite>(Resources.LoadAll<Sprite>("Foods"));
+        plate = GameObject.Find("plate").GetComponent<SpriteRenderer>();
+
+        //resetting the plate's sprite to none, just in case
+
+
+        Debug.Log(plate.name);
+        Debug.Log(foods[0].name);
+
         Ingreds[] i = new Ingreds[1];
 
         //for the first recipe
@@ -37,11 +51,18 @@ public class PlayerScript : MonoBehaviour {
         otherI[1] = Ingreds.Chicken;
 
         //loading all of the recipes; this will have to be done on a level-by level basis
-        recipes.Add(new Recipe("Chopped Carrots", i, CookingUten.Knife));
+        recipes.Add(new Recipe("Carrot Salad", i, CookingUten.Knife));
 
         recipes.Add(new Recipe("Carrot Soup", i, CookingUten.Stove));
 
-        recipes.Add(new Recipe("Chicken + Beef", otherI, CookingUten.Stove));
+        recipes.Add(new Recipe("Beef and Chicken", otherI, CookingUten.Stove));
+
+        //Adding the food names to allow us to search for the sprite
+        foreach (Sprite s in foods)
+        {
+            foodNames.Add(s.name);
+        }
+        
     }
 
 	// Update is called once per frame
@@ -51,32 +72,43 @@ public class PlayerScript : MonoBehaviour {
         //if enough time has passed, put the next item in the queue into the player's hand
         if (countdown <= 0.0f && playerQueue.Count > 0)
         {
-            countdown = 3.0f;
+            countdown = 1.0f;
             //putting the ingredient into the player's hand
             if (playerQueue.Peek().GetType() == typeof(Ingreds))
             {
                 itemsInHand.Add((Ingreds) playerQueue.Dequeue());
-                //Debug.Log("got ingredient");
             }
             //"using" the kitchen utensil.  must check if the recipe exists
             else
             {
                 Recipe r = getRecipe(itemsInHand.ToArray(), (CookingUten)playerQueue.Dequeue());
-                if (r != null)
-                {
-                    plateInHand = r;
-                }
-                else
-                {
-                    plateInHand = slop;
-                }
+
+                changePlateInHand(r);
+
                 //clearing the ingredients
                 itemsInHand.Clear();
             }
-            //to change the sprite of the plate the chefcat holds
-            this.GetComponentInChildren<SpriteRenderer>().sprite = Sprite;
+            
         }
 	}
+
+    private void changePlateInHand(Recipe r)
+    {
+        int index = 0;
+        if (r != null)
+        {
+            plateInHand = r;
+            //to change the sprite of the plate the chefcat holds
+            index = foodNames.IndexOf(r.getRecipeName());
+            plate.sprite = foods[index];
+        }
+        else
+        {
+            plateInHand = slop;
+            index = foodNames.IndexOf("None");
+            plate.sprite = foods[index];
+        }
+    }
 
     //allows the buttons from the crates/cooking utens to be added into the player queue
     public static void addToPlayerQueue(Ingreds i, CookingUten c)
@@ -103,12 +135,12 @@ public class PlayerScript : MonoBehaviour {
 
         Debug.Log("Doesn't match any recipe.");
 
-        return null;
+        return slop;
     }
 
     public void throwPlateAway()
     {
-        plateInHand = null;
+        changePlateInHand(null);
         Debug.Log("thrown away");
     }
 
@@ -126,7 +158,7 @@ public class PlayerScript : MonoBehaviour {
             if (cs[i].rightRecipe(plateInHand))
             {
                 Debug.Log("Yay! The customer got his meal!");
-                plateInHand = null;
+                changePlateInHand(null);
                 CustomerGenerator.removeCustomer(cs[i]);
                 return;
             }
