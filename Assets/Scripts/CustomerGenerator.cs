2ea71a92b2d780ee5e6f40dff069247a0820edb3
pbushko <pbushko@gmail.com>
2018-08-15 01:18:09 -0400

    Customer Generator working without creating a new prefab for each customer
    yay

--- Assets/Scripts/CustomerGenerator.cs
+++ Assets/Scripts/CustomerGenerator.cs
@@ -42,7 +42,7 @@ public class CustomerGenerator : MonoBehaviour {
             //if enough time has passed, get the next customer
             if (m_countdown <= 0.0f)
             {
-                m_countdown = 2.0f;
+                m_countdown = 5.0f;
                 AddCustomer();
             }
         }
@@ -66,38 +66,6 @@ public class CustomerGenerator : MonoBehaviour {
 
     private void AddCustomer()
     {
-        /*
-        //trying to make the customer prefab button get into line
-        GameObject c = (GameObject)Instantiate(buttonPrefab);
-        c.transform.SetParent(customerLine.transform);
-
-        //depending on what # customer this is, we want to offset the position
-        c.transform.position = 
-            new Vector3(m_linePosition.x + (m_customers.Count * Variables.CUSTOMER_OFFSET), m_linePosition.y + (m_customers.Count * Variables.CUSTOMER_OFFSET), m_linePosition.z);
-        
-        Customer temp = c.GetComponent<Customer>();
-        temp.m_number = m_customers.Count;
-        Recipe order = PlayerScript.GetRandomRecipe();
-        temp.m_order = order;
-        c.transform.GetChild(1).GetComponent<SpriteRenderer>().sprite = PlayerScript.GetFoodSprite(order);
-
-        //changing the style of the customer to be random
-        int rand = Random.Range(0, 2);
-        c.GetComponent<SpriteRenderer>().sprite = m_customerSprites[rand];
-        c.transform.GetChild(3).GetComponent<SpriteRenderer>().sprite = m_customerSprites[Random.Range(2, 4)];
-        //changing the patience
-        if (rand == 1)
-        {
-            temp.m_heartCount += rand;
-        }
-        else
-        {
-            temp.transform.GetChild(2).transform.GetChild(3).GetComponent<SpriteRenderer>().sprite = null;
-        }
-
-        m_customers.Add(c);
-        */
-
         //getting the customer that will be next in line
         GameObject newCustomer = m_customers[m_customerCount];
         Customer c = newCustomer.GetComponent<Customer>();
@@ -105,7 +73,6 @@ public class CustomerGenerator : MonoBehaviour {
         //setting the info for the customer
         c.m_number = m_customerCount;
         c.m_order = PlayerScript.GetRandomRecipe();
-        newCustomer.transform.GetChild(1).GetComponent<SpriteRenderer>().sprite = PlayerScript.GetFoodSprite(c.m_order);
 
         //setting the customer's body sprites
         int rand = Random.Range(0, 2);
@@ -113,9 +80,7 @@ public class CustomerGenerator : MonoBehaviour {
         Sprite face = m_customerSprites[Random.Range(2, 4)];
         c.body = body;
         c.face = face;
-        newCustomer.GetComponent<SpriteRenderer>().sprite = body;
-        newCustomer.transform.GetChild(3).GetComponent<SpriteRenderer>().sprite = face;
-
+        
         //changing the patience
         if (rand == 1)
         {
@@ -125,6 +90,8 @@ public class CustomerGenerator : MonoBehaviour {
         {
             c.transform.GetChild(2).transform.GetChild(3).GetComponent<SpriteRenderer>().enabled = false;
         }
+
+        SetCustomerSprites(newCustomer, c);
 
         m_customerCount++;
         newCustomer.SetActive(true);
@@ -138,42 +105,30 @@ public class CustomerGenerator : MonoBehaviour {
 
     public static void RemoveCustomer(int n)
     {
-        if (m_customerCount - 1 > 0)
+        m_customerCount--;
+        if (m_customerCount > 0 && n != m_customerCount)
         {
             ShiftPositions(n);
         }
-        m_customerCount--;
+        m_customers[m_customerCount].SetActive(false);
+        
     }
 
     private static void ShiftPositions(int n)
     {
-        /*
-        //just setting the 1st customer's position to the start of the line
-        m_customers[0].transform.position = m_linePosition;
-        m_customers[0].GetComponent<Customer>().SetCustomerNumber(0);
-        //already checked the 1st position
-        for (int i = 1; i < m_customers.Count; i++)
-        {
-            m_customers[i].GetComponent<Customer>().SetCustomerNumber(i);
-            //if the position is more than the offset, then it's not in the right spot
-            if (m_customers[i].transform.position.x > m_customers[i-1].transform.position.x + Variables.CUSTOMER_OFFSET)
-            {
-                //change all of the positions for the remaining buttons
-                while (i < m_customerCount)
-                {
-                    m_customers[i].GetComponent<Customer>().SetCustomerNumber(i);
-                    m_customers[i].transform.position = GetNewPosition(m_customers[i]);
-                    i++;
-                }
-            }
-        }*/
         //going up the row from the one removed to change the data
-        for (int i = n; n < m_customerCount; i++)
+        for (int i = n; i < m_customerCount; i++)
         {
-            Customer cur = m_customers[n].GetComponent<Customer>();
-            Customer next = m_customers[n+1].GetComponent<Customer>();
-            cur = next;
-            SetCustomerSprites(m_customers[n], next);
+            Customer cur = m_customers[i].GetComponent<Customer>();
+            Customer next = m_customers[i+1].GetComponent<Customer>();
+
+            cur.m_order = next.m_order;
+            cur.m_patience = next.m_patience;
+            cur.m_heartCount = next.m_heartCount;
+            cur.body = next.body;
+            cur.face = next.face;
+
+            SetCustomerSprites(m_customers[i], next);
         }
     }
 
