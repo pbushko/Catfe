b43168ed77fe027e6d39560bdc37184921f8bc1e
Priscilla <pbushko@gmail.com>
2019-03-07 17:42:34 -0500

    Pushing the new popup with its script along with new textmeshpro things

---
+++ Assets/Scripts/ConfirmationPopup.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ConfirmationPopup : MonoBehaviour
+{
+    public TextMeshProUGUI decorName;
+    public TextMeshProUGUI decorCost;
+    public TextMeshProUGUI decorAtmosphere;
+    public TextMeshProUGUI decorLocation;
+    public TextMeshProUGUI decorDescription;
+    public TextMeshProUGUI upgradeName;
+    public TextMeshProUGUI upgradeCost;
+    public TextMeshProUGUI upgradeCookTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetDecorationText(DecorationData d)
+    {
+        decorName.text = "" + d.name;
+        decorCost.text = "Cost: " + d.cost;
+        decorAtmosphere.text = "Atmosphere: " + d.atmosphere;
+        decorLocation.text = "Location: " + d.location;
+        decorDescription.text = "" + d.description;
+    }
+}
